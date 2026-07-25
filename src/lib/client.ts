9e1209745d74e3ed8344611a9b62f80f53e6a4a1
Adonis <adonisoussou737@gmail.com>
2026-07-26 00:21:11 +0100

    fix: add 3s timeout to GraphQL queries for faster fallback loading

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,6 +2,11 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client/core'
 
 const httpLink = new HttpLink({
   uri: import.meta.env.VITE_WPGRAPHQL_ENDPOINT || 'https://allianceactionsafrique.com/graphql',
+  fetch: (uri, options) => {
+    const controller = new AbortController()
+    const timeout = setTimeout(() => controller.abort(), 3000)
+    return fetch(uri, { ...options, signal: controller.signal }).finally(() => clearTimeout(timeout))
+  },
 })
 
 export const apolloClient = new ApolloClient({
